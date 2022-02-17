f88e02c10a8a47248857908a25d31f3e6c4ba059
Gabriel Takaki <gabitj@icloud.com>
2022-02-17 10:22:26 -0300

    user schema

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,11 +1,12 @@
 import { Express, Request, Response } from "express";
 import { createHandler } from "./controller/user.controller";
 import validate from './middleware/validate';
+import { userSchema } from "./schema/user.schema";
 
 export default function (app: Express) {
   app.get('/', (req: Request, res: Response) => {
     res.sendStatus(200);
   });
 
-  app.post('/users', validate(), createHandler)
+  app.post('/users', validate(userSchema), createHandler)
 }
