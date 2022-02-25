dbf2f046760491736870fa0a03ab2acebf080ee3
Gabriel Takaki <gabitj@icloud.com>
2022-02-25 17:36:13 -0300

    slider

--- client/src/screens/intials/Home.tsx
+++ client/src/screens/intials/Home.tsx
@@ -2,15 +2,17 @@ import React, { useContext, useEffect } from 'react';
 import '../../styles/style.css';
 import { Global } from '../../context';
 import Header from '../../components/Header';
+import SwiperCore, { Autoplay } from 'swiper'
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { BsCart3 } from 'react-icons/bs';
 
 // import Swiper styles
 import 'swiper/css';
-import 'swiper/css/effect-fade';
+import 'swiper/css/virtual';
 
 const Home = () => {
   const { getProducts, products } = useContext(Global.Context);
+  SwiperCore.use([Autoplay])
 
   useEffect(() => {
     getProducts();
@@ -23,14 +25,15 @@ const Home = () => {
     <>
       <Header />
       <Swiper
-        className="swiper"
-        lazy={true}
-        pagination={{ clickable: true }}
-        scrollbar={{ draggable: true }}
-      >
+        loop={true}
+        speed={900}
+        autoplay={{
+          delay: 2000,
+        disableOnInteraction: false,
+      }}>
         {
           products && products.map((product, index) => index < 3 && (
-              images.map((image) => (
+              images.map((image, index) => (
                 <SwiperSlide className="section">
                   <img className="section__image" src={ image.src } alt="iphone" />
                   <h1 className="section__title">{product.name}</h1>
