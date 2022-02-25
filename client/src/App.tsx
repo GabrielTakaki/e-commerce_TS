bb0030b80ee3df9a5811d9c9e4b38ccc092a6ba1
Gabriel Takaki Junkes <gabitj@icloud.com>
2022-02-25 10:55:09 -0300

    Register endpoint - Home / Register routing (#10)
    * file upload w/ multer
    
    * front end initial setup
    
    * context inital setup
    
    * regsiter endpoint and Home, Register routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,14 +2,26 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { Global } from './context';
 
+import Register from './screens/intials/Register';
 import Login from './screens/intials/Login';
+import Home from './screens/intials/Home';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={
         <Global.Provider>
+          <Home />
+        </Global.Provider>
+      } />
+      <Route path="/login" element={
+        <Global.Provider>
           <Login />
+        </Global.Provider>
+      } />
+      <Route path="/register" element={
+        <Global.Provider>
+          <Register />
         </Global.Provider>
       } />
     </Routes>
