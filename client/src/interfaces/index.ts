0882d415718606ab1a961583ffa5013645a38af0
Gabriel Takaki <gabitj@icloud.com>
2022-02-25 14:00:20 -0300

    image carousel

--- client/src/interfaces/index.ts
+++ client/src/interfaces/index.ts
@@ -8,3 +8,24 @@ export interface IUserRegister {
   role: string;
   name: string;
 };
+
+export interface IUser {
+  email: string;
+  password: string;
+}
+
+export interface IProducts {
+  name: string;
+  price: number;
+  quantity: number;
+  description: string;
+  image?: string;
+}
+
+export interface IContext {
+  user: IUserRegister[];
+  products: IProducts[];
+  login: (email: string, password: string) => Promise<any>;
+  register: (email: string, password: string, role: string, name: string) => Promise<any>;
+  getProducts: () => Promise<any>;
+}
--- client/src/screens/intials/Home.tsx
+++ client/src/screens/intials/Home.tsx
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import '../../styles/style.css';
+import { Global } from '../../context';
 import Header from '../../components/Header';
+import { Swiper, SwiperSlide } from 'swiper/react';
+// import Swiper core and required modules
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+
+// import Swiper styles
+import 'swiper/css';
 
 const Home = () => {
+  const { getProducts, products } = useContext(Global.Context);
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  const images = [
+    { src: 'https://pngimg.com/uploads/iphone_13/iphone_13_PNG31.png' },
+  ]
   return (
-    <div>
+    <>
       <Header />
-    </div>
+      <Swiper
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+      >
+        {
+          products && products.map((product, index) => index < 3 && (
+              images.map((image) => (
+                <SwiperSlide className="section">
+                  <img className="section__image" src={ image.src } alt="iphone" />
+                  <h1 className="section__title">{product.name}</h1>
+                  <p className="section__desc">{product.description}</p>
+                  <p className="section__price">{product.price}</p>
+                  <button>
+                    Buy Now
+                  </button>
+                </SwiperSlide>
+              ))
+          ))
+        }
+      </Swiper>
+    </>
   );
 }
 
