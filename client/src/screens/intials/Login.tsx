fe88c7e094706590733b02b9aa1d97438e28c7c5
Gabriel Takaki <gabitj@icloud.com>
2022-02-26 17:42:18 -0300

    image in bigger screens

--- client/src/screens/intials/Login.tsx
+++ client/src/screens/intials/Login.tsx
@@ -5,7 +5,12 @@ const Login: React.FC = () => {
   const [password, setPassword] = useState('');
   return (
     <div className="container-login">
+      <div className="greeting">
+        <h3 className="greeting__greet">Welcome to</h3>
+        <h3 className="greeting__title">GT. Commerce</h3>
+      </div>
       <form className="login">
+        <p className="login__title">Login</p>
         <label htmlFor="email" className="login__label">
           email
           <input
@@ -26,7 +31,9 @@ const Login: React.FC = () => {
             onChange={ (e) => setPassword(e.target.value) }
           />
         </label>
+        <button className="login__button" type="button">Login</button>
       </form>
+      <img src="https://www.pngmart.com/files/11/E-Commerce-PNG-Free-Download.png" alt="e-commerce" className="login__img" />
     </div>
   );
 }
