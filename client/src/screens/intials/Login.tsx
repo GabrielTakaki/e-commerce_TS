083c15cf01a1d7d4a249bd9f8c2d24b6f6c361a0
Gabriel Takaki <gabitj@icloud.com>
2022-02-26 17:18:19 -0300

    inital styling to login page

--- client/src/screens/intials/Login.tsx
+++ client/src/screens/intials/Login.tsx
@@ -4,20 +4,22 @@ const Login: React.FC = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   return (
-    <div>
-      <form>
-        <label htmlFor="email">
+    <div className="container-login">
+      <form className="login">
+        <label htmlFor="email" className="login__label">
           email
           <input
+            className="login__input"
             type="email"
             name="email"
             value={ email }
             onChange={ (e) => setEmail(e.target.value) }
           />
         </label>
-        <label htmlFor="password">
+        <label htmlFor="password" className="login__label">
           password
           <input
+            className="login__input"
             type="password"
             name="password"
             value={ password }
