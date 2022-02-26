160d388d508a04708bcee8b0abc9af4c0aa9553f
Gabriel Takaki <gabitj@icloud.com>
2022-02-26 17:02:35 -0300

    login inital setup

--- client/src/screens/intials/Login.tsx
+++ client/src/screens/intials/Login.tsx
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Login: React.FC = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   return (
-    <div>Login</div>
+    <div>
+      <form>
+        <label htmlFor="email">
+          email
+          <input
+            type="email"
+            name="email"
+            value={ email }
+            onChange={ (e) => setEmail(e.target.value) }
+          />
+        </label>
+        <label htmlFor="password">
+          password
+          <input
+            type="password"
+            name="password"
+            value={ password }
+            onChange={ (e) => setPassword(e.target.value) }
+          />
+        </label>
+      </form>
+    </div>
   );
 }
 
