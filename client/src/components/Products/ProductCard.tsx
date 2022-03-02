44328d9322f907d529f37105ba507f3dbcdeb00c
Gabriel Takaki <gabitj@icloud.com>
2022-03-02 12:04:49 -0300

    starts the product card component

--- client/src/components/Products/ProductCard.tsx
+++ client/src/components/Products/ProductCard.tsx
@@ -8,10 +8,29 @@ const ProductCard: React.FC = () => {
     getProducts();
   }, []);
 
+  const images = [
+    { src: 'https://pngimg.com/uploads/iphone_13/iphone_13_PNG29.png' },
+    { src: 'https://www.pngmart.com/files/15/Apple-iPhone-11-PNG-Picture.png' },
+    { src: 'https://www.freeiconspng.com/uploads/new-iphone-x-photo-18.png' }
+  ];
+
+  const productWithImage = products && products.map((item, idx) => ({
+    ...item,
+    image: images[idx]
+  }));
+
   return (
-    <div>
-      oi
-    </div>
+    <>
+      {
+        productWithImage && productWithImage.map((product, index) => index < 3 && (
+          <div key={index}>
+            <img src={ product.image.src } alt="product" />
+            <h4>{product.name}</h4>
+            <p>{product.price}</p>
+          </div>
+        ))
+      }
+    </>
   );
 }
 
